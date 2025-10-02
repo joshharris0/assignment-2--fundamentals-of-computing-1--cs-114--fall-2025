b7eaac1dbb660bf20bfb48c72d6f6a81d6ab10e8
joshharris0 <joshharri@hartford.edu>
2025-10-02 16:50:11 -0400

    Update CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,17 @@
 public class CountFlips {
   public static void main(String[] args) {
-  }
+    Coin myCoin = new Coin();
+    int tails = 0;
+    int heads = 0;
+    System.out.println("Flipped 100x");
+    for (int i = 0; i < 100; i++){
+       myCoin.flip();
+        if (myCoin.isHeads())
+        heads++;
+      else
+        tails++;
+    }
+        System.out.println("# of heads flipped: " + heads);
+        System.out.println("# of tails flipped: " + tails);
+    }
 }
